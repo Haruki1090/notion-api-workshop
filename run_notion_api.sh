a1328415735f3c7c5b83f183f07a09b4067347af
Haruki Inoue <151597393+Haruki1090@users.noreply.github.com>
2024-12-14 06:41:05 +0000

    NotionAPIをターミナルで叩くコマンド

---
+++ run_notion_api.sh
@@ -0,0 +1,5 @@
+curl -X POST 'https://api.notion.com/v1/databases/データベースID/query' \
+-H 'Authorization: Bearer APIトークン' \
+-H 'Content-Type: application/json' \
+-H 'Notion-Version: 2022-06-28' \
+--data '{}'
